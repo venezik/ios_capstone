5bbac0c8c0f3898a813db5d227015d13ff8eb83b
Yaroslav Petrunin <iaroslavpetrunin@gmail.com>
2024-04-16 00:47:36 -0400

    init

---
+++ FitLife/FitLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitLife
+//
+//  Created by Yaroslav on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
