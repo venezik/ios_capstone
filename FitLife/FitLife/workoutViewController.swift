d6aee301e33f89bf9d74791c137111d577892379
Yaroslav Petrunin <iaroslavpetrunin@gmail.com>
2024-04-23 09:40:18 -0400

    Retrieving work done

---
+++ FitLife/FitLife/workoutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  workoutViewController.swift
+//  FitLife
+//
+//  Created by Yaroslav on 4/23/24.
+//
+
+import UIKit
+
+class workoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FitLife/NutritionCell.swift
@@ -0,0 +1,23 @@
+//
+//  NutritionCell.swift
+//  FitLife
+//
+//  Created by Yaroslav on 4/16/24.
+//
+
+import UIKit
+
+class NutritionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
